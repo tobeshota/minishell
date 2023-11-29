2fb265d24b7ac005b6cebe5a3693025bc67cca41
戸部翔太 <tobeshouta@Intel.local>
2023-11-29 11:23:30 +0900

    [env] show only env content with definition

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -6,11 +6,16 @@
 /*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 11:39:21 by toshota           #+#    #+#             */
-/*   Updated: 2023/11/25 22:40:18 by toshota          ###   ########.fr       */
+/*   Updated: 2023/11/29 11:23:24 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+
+static bool	is_identifier_definition_exist(char *content)
+{
+	return (ft_strchr(content, '='));
+}
 
 int	exec_env(t_env **env, t_pipex *pipex)
 {
@@ -18,10 +23,12 @@ int	exec_env(t_env **env, t_pipex *pipex)
 		return (false);
 	while ((*env)->next)
 	{
-		ft_putendl_fd((*env)->content, pipex->outfile_fd);
+		if (is_identifier_definition_exist((*env)->content))
+			ft_putendl_fd((*env)->content, pipex->outfile_fd);
 		ft_nodenext(env);
 	}
-	ft_putendl_fd((*env)->content, pipex->outfile_fd);
+	if (is_identifier_definition_exist((*env)->content))
+		ft_putendl_fd((*env)->content, pipex->outfile_fd);
 	ft_nodefirst(env);
 	return (true);
 }
