dd20631f7b13fce51173c339af3e93dd599aac27
JIA0010 <HIROsroutine@gmail.com>
2023-11-30 16:12:41 +0900

    a

--- srcs/pipex/is_file_ok.c
+++ srcs/pipex/is_file_ok.c
@@ -6,7 +6,7 @@
 /*   By: yoshimurahiro <yoshimurahiro@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 12:14:49 by toshota           #+#    #+#             */
-/*   Updated: 2023/11/30 15:52:20 by yoshimurahi      ###   ########.fr       */
+/*   Updated: 2023/11/30 16:12:35 by yoshimurahi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 bool	is_file_exist(char *file)
 {
-	ft_printf("%s\t%d\n", file, !access(file, F_OK));
-
 	return (!access(file, F_OK));
 }
 
@@ -31,7 +29,5 @@ bool	is_file_writable(char *file)
 
 bool	is_file_exectable(char *file)
 {
-	ft_printf("%s\t%d\n", file, !access(file, X_OK));
-	
 	return (!access(file, X_OK));
 }
