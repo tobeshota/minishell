2dd3aefd13dfbc57123750efa4966131eaf9644d
戸部翔太 <tobeshouta@Intel.local>
2023-12-02 20:38:06 +0900

    [pipex] I avoid causing SEGV when only redirect operater passes to pipex

--- srcs/pipex/get_fd.c
+++ srcs/pipex/get_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_fd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 01:22:30 by toshota           #+#    #+#             */
-/*   Updated: 2023/12/01 16:47:59 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/02 20:37:56 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,14 @@ bool	get_infile_fd(t_pipex *pipex, int cmd_i, char **argv, int exec_flag)
 	pipex->infile_fd = STDIN_FILENO;
 	while (argv[arg_i] && !is_specified_pipe(argv[arg_i]))
 	{
-		if (is_specified_infile(argv[arg_i]))
+		if (is_specified_infile(argv[arg_i]) && argv[arg_i + 1])
 		{
 			pipex->infile_fd = open_file(argv[arg_i + 1], INFILE);
 			if (check_open(pipex->infile_fd) == false)
 				return (false);
 		}
-		else if (is_specified_here_doc(argv[arg_i]) && exec_flag == true)
+		else if (is_specified_here_doc(argv[arg_i]) && \
+		argv[arg_i + 1] && exec_flag == true)
 			if (proc_here_doc(argv[arg_i + 1], pipex) == false)
 				return (false);
 		arg_i++;
@@ -56,10 +57,10 @@ bool	get_outfile_fd(t_pipex *pipex, int cmd_i, char **argv)
 	pipex->outfile_fd = STDOUT_FILENO;
 	while (argv[arg_i] && !is_specified_pipe(argv[arg_i]))
 	{
-		if (is_specified_outfile_overwriting(argv[arg_i]))
+		if (is_specified_outfile_overwriting(argv[arg_i]) && argv[arg_i + 1])
 			pipex->outfile_fd = \
 			open_file(argv[arg_i + 1], OUTFILE_OVER_WRITING);
-		else if (is_specified_outfile_apend(argv[arg_i]))
+		else if (is_specified_outfile_apend(argv[arg_i]) && argv[arg_i + 1])
 			pipex->outfile_fd = \
 			open_file(argv[arg_i + 1], OUTFILE_APEND);
 		if (check_open(pipex->outfile_fd) == false)
