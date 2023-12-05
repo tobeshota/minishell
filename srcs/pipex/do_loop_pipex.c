1c32b3fab42e3ecf3ad16b377c12150e79af6dd0
toshota <toshota@student.42tokyo.jp>
2023-12-05 15:28:17 +0900

    [pipex] implement exit status: 0, 1, 126, 128

--- srcs/pipex/do_loop_pipex.c
+++ srcs/pipex/do_loop_pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   do_loop_pipex.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 12:49:44 by toshota           #+#    #+#             */
-/*   Updated: 2023/12/05 11:46:07 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/05 15:18:59 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 static int	is_continue(char **splitter, int spl_i, int ret)
 {
 	return (is_match(splitter[spl_i], ";") \
-	|| (is_match(splitter[spl_i], "&&") && ret == true) \
-	|| (is_match(splitter[spl_i], "||") && ret != true));
+	|| (is_match(splitter[spl_i], "&&") && ret == 0) \
+	|| (is_match(splitter[spl_i], "||") && ret != 0));
 }
 
 int	do_loop_pipex(char ***splitted_argv, char **splitter, char **envp,
@@ -30,16 +30,17 @@ int	do_loop_pipex(char ***splitted_argv, char **splitter, char **envp,
 	spl_i = 0;
 	while (splitted_argv[++sparg_i])
 	{
+		g_global.error_num = 0;
 		node_to_array(*env, &envp);
 		ret = pipex(splitted_argv[sparg_i], env);
 		if (splitter[spl_i] == NULL)
 			break ;
-		if (is_continue(splitter, spl_i, ret) && ++spl_i)
+		if (is_continue(splitter, spl_i, g_global.error_num) && ++spl_i)
 			continue ;
-		else if (is_match(splitter[spl_i], "&&") && ret != true)
+		else if (is_match(splitter[spl_i], "&&") && g_global.error_num != 0)
 			while (is_match(splitter[spl_i], "&&") && ++sparg_i)
 				spl_i++;
-		else if (is_match(splitter[spl_i], "||") && ret == true)
+		else if (is_match(splitter[spl_i], "||") && g_global.error_num == 0)
 			while (is_match(splitter[spl_i], "||") && ++sparg_i)
 				spl_i++;
 	}
--- srcs/pipex/loop_pipex.c
+++ srcs/pipex/loop_pipex.c
@@ -3,30 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   loop_pipex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 09:23:37 by toshota           #+#    #+#             */
-/*   Updated: 2023/12/05 11:45:39 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/05 15:27:44 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
 // cat infile,|,cat,|,cat,|,cat,|,cat,;,echo -n wow,;,pwd
-// b.outがcommand not foundとであるが，pipex();の返り値はtrueとなってしまっているため,echo wowが実行されない．終了ステータスを直す！
 // pwd,&&,b.out,||,echo wow
 int	loop_pipex(char **argv, char **envp, t_env **env)
 {
 	char	***splitted_argv;
 	char	**splitter;
-	int		ret;
 
 	if (is_splitter_exist(argv) == false)
 	{
-		ret = pipex(argv, env);
-		return (node_to_array(*env, &envp), ret);
+		pipex(argv, env);
+		return (node_to_array(*env, &envp), g_global.error_num);
 	}
 	get_loop_pipex(argv, &splitted_argv, &splitter);
-	ret = do_loop_pipex(splitted_argv, splitter, envp, env);
-	return (end_loop_pipex(splitted_argv, splitter), ret);
+	do_loop_pipex(splitted_argv, splitter, envp, env);
+	return (end_loop_pipex(splitted_argv, splitter), g_global.error_num);
 }
