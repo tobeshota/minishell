756c252a9ec8068f000d51438837a35d7f3dd17e
toshota <toshota@student.42tokyo.jp>
2023-12-06 17:05:19 +0900

    [pipex] fix do_loop_pipx

--- srcs/pipex/do_loop_pipex.c
+++ srcs/pipex/do_loop_pipex.c
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 12:49:44 by toshota           #+#    #+#             */
-/*   Updated: 2023/12/05 15:18:59 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/06 17:04:43 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ int	do_loop_pipex(char ***splitted_argv, char **splitter, char **envp,
 	int	spl_i;
 	int	ret;
 
-	sparg_i = -1;
+	sparg_i = 0;
 	spl_i = 0;
-	while (splitted_argv[++sparg_i])
+	while (splitted_argv[sparg_i])
 	{
 		g_global.error_num = 0;
 		node_to_array(*env, &envp);
 		ret = pipex(splitted_argv[sparg_i], env);
 		if (splitter[spl_i] == NULL)
 			break ;
-		if (is_continue(splitter, spl_i, g_global.error_num) && ++spl_i)
+		if (is_continue(splitter, spl_i, g_global.error_num) && ++spl_i && ++sparg_i)
 			continue ;
 		else if (is_match(splitter[spl_i], "&&") && g_global.error_num != 0)
 			while (is_match(splitter[spl_i], "&&") && ++sparg_i)
@@ -43,6 +43,9 @@ int	do_loop_pipex(char ***splitted_argv, char **splitter, char **envp,
 		else if (is_match(splitter[spl_i], "||") && g_global.error_num == 0)
 			while (is_match(splitter[spl_i], "||") && ++sparg_i)
 				spl_i++;
+		if (splitted_argv[sparg_i] == NULL)
+			break ;
+		sparg_i++;
 	}
 	return (ret);
 }
