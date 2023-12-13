8e61644d1089aa53a7da5750d2ff50104b7eebac
戸部翔太 <tobeshouta@Intel.local>
2023-12-13 21:07:05 +0900

    [pipex] rm double declaration about omit

--- srcs/builtin/exec_export/get_old_env_to_be_updated.c
+++ srcs/builtin/exec_export/get_old_env_to_be_updated.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_old_env_to_be_updated.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 23:29:27 by toshota           #+#    #+#             */
-/*   Updated: 2023/12/11 12:43:10 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/13 21:05:22 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,58 +48,4 @@ t_env	*get_old_env_to_be_updated(char *added, t_env *env)
 	}
 	return (free(added_identifier), free(env_identifier), ft_nodefirst(&env),
 		NULL);
-}
-
-static size_t	strlen_without_c(char *str, char c)
-{
-	int	i;
-	int	len_without_c;
-
-	i = 0;
-	len_without_c = 0;
-	while (str[i])
-	{
-		if (str[i] != c)
-			len_without_c++;
-		i++;
-	}
-	return (len_without_c);
-}
-
-char	*omit_c(char *str, char c)
-{
-	char	*str_without_c;
-	int		i;
-
-	str_without_c = (char *)check_malloc(malloc(sizeof(char)
-				* (strlen_without_c(str, c) + 1)));
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] != c)
-		{
-			str_without_c[i] = str[i];
-			i++;
-		}
-		else
-			str++;
-	}
-	str_without_c[i] = '\0';
-	return (str_without_c);
-}
-
-char	*omit_str(char *str, char *omit)
-{
-	char	*tmp;
-	char	*result;
-
-	result = check_malloc(ft_strdup(str));
-	while (*omit)
-	{
-		tmp = result;
-		result = omit_c(result, *omit);
-		free(tmp);
-		omit++;
-	}
-	return (result);
 }
