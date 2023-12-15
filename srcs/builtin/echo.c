1197ee75aabadf5f7ceb8a7e9a3e16072ed556eb
戸部翔太 <tobeshouta@Intel.local>
2023-12-15 23:29:44 +0900

    [echo] fix echo option

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -6,17 +6,31 @@
 /*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 11:39:21 by toshota           #+#    #+#             */
-/*   Updated: 2023/11/25 22:40:18 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/15 23:29:33 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+static bool	is_optin_specified(char *option, char option_name)
+{
+	int	i;
+
+	if (option[0] != '-')
+		return (false);
+	if (option[1] != option_name)
+		return (false);
+	i = 2;
+	while (option[i] && option[i] == option_name)
+		i++;
+	return (option[i] == '\0');
+}
+
 int	exec_echo(char **cmd, t_pipex *pipex)
 {
 	int	cmd_i;
 
-	if (cmd[1] && is_match(cmd[1], "-n") == true)
+	if (cmd[1] && is_optin_specified(cmd[1], 'n') == true)
 	{
 		cmd_i = 2;
 		while (cmd[cmd_i])
