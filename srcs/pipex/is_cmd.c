48932965b0b5d167445e969f6e98c1aba509ac52
戸部翔太 <tobeshouta@Intel.local>
2023-12-16 11:07:41 +0900

    [pipex] end process when something error msg puts

--- srcs/pipex/is_cmd.c
+++ srcs/pipex/is_cmd.c
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 23:22:47 by toshota           #+#    #+#             */
-/*   Updated: 2023/12/10 22:27:11 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/16 00:17:48 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,31 @@ static int	is_file_exectable_wo_additional_path(char *file)
 	|| (is_file_exectable(file) && ft_strchr(file, '/')));
 }
 
-static int	is_false(char **argv, int arg_i)
-{
-	return (is_specified_operators(argv[arg_i]) || is_io_file(argv, arg_i));
-}
-
 static char	*get_argv_wo_param(char **argv, int arg_i)
 {
 	return (check_malloc \
 	(ft_substr(argv[arg_i], 0, strlen_until_c(argv[arg_i], ' '))));
 }
 
+static int	is_false(char **argv, int arg_i)
+{
+	char	*argv_wo_param;
+	char	*prev_argv_wo_param;
+
+	argv_wo_param = get_argv_wo_param(argv, arg_i);
+	if (is_specified_operators(argv_wo_param))
+		return (free(argv_wo_param), true);
+	if (arg_i > 0)
+	{
+		prev_argv_wo_param = get_argv_wo_param(argv, arg_i - 1);
+		if (is_io_file(argv_wo_param, prev_argv_wo_param))
+			return (free(argv_wo_param), free(prev_argv_wo_param), true);
+		free(prev_argv_wo_param);
+	}
+	return (free(argv_wo_param), false);
+}
+
+/* ls,|," cat"に対応する！ */
 int	is_cmd(char **argv, int arg_i, char **h_envp)
 {
 	char	*argv_wo_param;
@@ -55,7 +69,7 @@ int	is_cmd(char **argv, int arg_i, char **h_envp)
 	else if (is_false(argv, arg_i))
 		return (free_tab(argv_wo_param), false);
 	else if (getenv_from_h_envp(h_envp, "PATH") == NULL
-		|| is_match(argv_wo_param, ".."))
+		|| is_match(argv_wo_param, "..") || ft_strlen(argv_wo_param) == 0)
 		return (free_tab(argv_wo_param), NOT_FOUND);
 	path = check_malloc \
 	(add_slash_eos(ft_split(getenv_from_h_envp(h_envp, "PATH"), ':')));
