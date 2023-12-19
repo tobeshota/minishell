65a83a62a7b531f908b0397c0baffad81928566e
戸部翔太 <tobeshouta@Intel.local>
2023-12-19 22:03:59 +0900

    [pipex] not to SEGV when unexist multiple variable is passed like ```$UNEXIST1 $UNXIST2```

--- srcs/pipex/loop_pipex.c
+++ srcs/pipex/loop_pipex.c
@@ -3,14 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   loop_pipex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 09:23:37 by toshota           #+#    #+#             */
-/*   Updated: 2023/12/19 14:44:28 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/19 22:00:12 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static bool	is_argv_exist(char **argv)
+{
+	char	*argv_wo_space;
+
+	if (ft_strlen(argv[0]) == 0)
+		return (false);
+	argv_wo_space = check_malloc(ft_strtrim(argv[0], " "));
+	if (ft_strlen(argv_wo_space) == 0)
+		return (free(argv_wo_space), false);
+	return (free(argv_wo_space), true);
+}
 
 int	loop_pipex(t_tools *tools, t_env **env)
 {
@@ -18,7 +30,7 @@ int	loop_pipex(t_tools *tools, t_env **env)
 	char	**splitter;
 	char	**h_envp;
 
-	if (ft_strlen(*tools->tmp_array) == 0)
+	if (is_argv_exist(tools->tmp_array) == false)
 		return (true);
 	if (is_splitter_exist(tools->tmp_array) == false)
 	{
