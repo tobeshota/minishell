a8bc2d420859a74d5607c7693c7b62bd94c09722
toshota <toshota@student.42tokyo.jp>
2023-12-28 14:58:06 +0900

    [env] refactoring

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 11:39:21 by toshota           #+#    #+#             */
-/*   Updated: 2023/11/29 11:23:24 by toshota          ###   ########.fr       */
+/*   Updated: 2023/12/28 14:57:41 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,13 @@ int	exec_env(t_env **env, t_pipex *pipex)
 {
 	if (env == NULL)
 		return (false);
-	while ((*env)->next)
+	while (true)
 	{
 		if (is_identifier_definition_exist((*env)->content))
 			ft_putendl_fd((*env)->content, pipex->outfile_fd);
+		if ((*env)->next == NULL)
+			break ;
 		ft_nodenext(env);
 	}
-	if (is_identifier_definition_exist((*env)->content))
-		ft_putendl_fd((*env)->content, pipex->outfile_fd);
-	ft_nodefirst(env);
-	return (true);
+	return (ft_nodefirst(env), true);
 }
